9577554349eca4cb4fa68537e6389a02adbdd484
Ying Cheng <yingyingch@yahoo.co.uk>
2023-01-20 15:50:48 +0000

    with README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Email Extraction
+
+This project will open a file and read the content, it will then count how many valid email addresses that contain "@softwire.com" and print the result. 
+
+## Setup
+
+Just run `npm install` to set up the dependencies, then `npm start` to run the project. 
--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 import fs from 'fs';
 const content = fs.readFileSync('test.txt', { encoding: 'utf-8' });
 
+//another way is 
+//const content=fs.readFileSync('test.txt);
+//console.log(content.toString());
+//read the file content to string
+
 const contentArray = content.split(' ');
 var counter = 0;
+const emailAddRegExp = /(@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/g;
+const domainRegExp = /\@softwire\.com\b/;
+//const domainRegExp = /@softwire\.com\b/gi;
+//  /g  global, all matches, not the first match
+//  /i  insensitive, case insensitive  \b word boundary
 
 for (let input of contentArray) {
 
-    for (var i = 0; i < input.length; i++) {
-        if (input[i] === '\@') {
-            if (input.substring(i) === "@softwire.com") {
-                counter++;
-            }
-
-        }
+    if (input.match(emailAddRegExp) && input.match(domainRegExp)) {
+        console.log(input.match(domainRegExp));
+        counter++;
     }
-
 
 }
 console.log(counter);
